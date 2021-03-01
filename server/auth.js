c6a8f74063578d923e7176548922e78f12ce7218
torinberger <torinberger@torinberger.com>
2021-03-01 14:02:42 +1100

    Integrated database controllers into passport authentication

--- server/auth.js
+++ server/auth.js
@@ -5,29 +5,47 @@ const db = require('./database');
 const handleErr = require('./err');
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user.username);
 });
 
-passport.deserializeUser((id, done) => {
-  User.findById(id, function (err, user) {
-    handleErr(err);
-    done(err, user);
-  });
+passport.deserializeUser((username, done) => {
+  db.appusers.findByUsername(username)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      handleErr(err);
+      done(err, null);
+    });
 });
+
+passport.use(new LocalStrategy((username, password, done) => {
+  db.appusers.findUserByCredentials(username, password)
+    .then((user) => {
+      if (!user) {
+        return done(null, false, { message: 'Incorrect credentials.' });
+      } else {
+        return done(null, user);
+      }
+    })
+    .catch((err) => {
+      handleErr(err);
+      done(err);
+    });
+}));
 
 passport.use(new LocalStrategy(options, (username, password, done) => {
-  db.appusers
-    .findUserByCredentials(username, password)
-      .then((user) => {
-        if (!user) return done(null, false);
-        if (password === user.password) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-        }
-      })
-      .catch((err) => {
-        handleErr(err);
-        return done(err);
-      });
+  db.appusers.findUserByCredentials(username, password)
+    .then((user) => {
+      if (!user) return done(null, false);
+      if (password === user.password) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => {
+      handleErr(err);
+      return done(err);
+    });
 }));
